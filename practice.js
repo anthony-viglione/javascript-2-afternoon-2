e44da0047cb9c410f29fb8deb7717534fe6b3086
Anthony Viglione <viglione2014.2015@gmail.com>
2019-01-29 15:38:59 -0700

    Completed Required Problems

--- practice.js
+++ practice.js
@@ -18,7 +18,7 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){return arr[0]}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +33,7 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){return arr[arr.length-1]}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,6 +49,19 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+var looper= family => {
+  for(i=0;i<family.length;i++){
+    window.alert(family[i])
+  }
+}
+
+      // window.alert("alert")
+
+// function looper(family){
+//   for (i=0;i<family.length;i++){
+//     window.alert(family[i])
+//   }
+// }
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +76,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(i=letters.length-1;i>=0;i--)
+  {
+    window.alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +96,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  var evens=[]
+  for(i=0;i<nums.length;i++){
+    if(nums[i]%2===0){
+      evens.push(nums[i])
+    }
+  }
+  return evens
+}
 
 
 
